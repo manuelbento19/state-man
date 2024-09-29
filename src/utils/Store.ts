a2e21674479ef3fa3c6d36e0745498f5d07d1dc7
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-29 22:00:50 +0100

    feat: store handler

---
+++ src/utils/Store.ts
@@ -0,0 +1,38 @@
+import { useSyncExternalStore } from "react";
+import { Store } from "../types";
+import { Observable } from "./Observable";
+
+
+export function createStore<T>(data: T): Store<T> {
+    const observable = new Observable();
+    let initial = data;
+
+    function set(data_:T){
+        initial = data_;
+        observable.notify();
+    }
+
+    function get(){
+        return initial;
+    }
+
+    return {
+        set,get,
+        subscribe: observable.subscribe.bind(observable)
+    }
+}
+
+
+export function useStore<T>(store:Store<T>){
+
+    const state = useSyncExternalStore(
+        store.subscribe,
+        store.get
+    )
+
+    return {
+        state,
+        setState: store.set
+    }
+
+} 
