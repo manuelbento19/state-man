860db4d9e5380678fb542849cb8f330af1094ef4
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-11-15 11:43:36 +0100

    refactor: identifier persisted data

--- src/utils/persist.test.ts
+++ src/utils/persist.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, test } from "vitest";
 import { persist } from "./persist";
+import { create } from "./store";
+import { renderHook } from "@testing-library/react";
 
 describe("Persisting store data",()=>{
     test("should create persisted data",()=>{
@@ -13,5 +15,19 @@ describe("Persisting store data",()=>{
         expect(persistedData.store).toBeDefined();
         expect(persistedData.setItem).toBeDefined();
         expect(persistedData.observable).toBeDefined();
+    })
+    test("should create store with persisted data",()=>{
+        const persistedData = persist({
+            data: 1,
+            name:"count",
+        })
+
+        const useStore = create(persistedData);
+
+        expect(useStore).toBeTruthy()
+        const {result: {current}} = renderHook(()=>useStore())
+        const {state} = current;
+
+        expect(state).toEqual(1);
     })
 })
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,11 +1,11 @@
 import { useSyncExternalStore } from "react";
 import { Observable } from "./observable";
-import { Observer, Setter } from "../types";
-import { IStore } from "interfaces";
+import { Observer, Persist, Setter } from "../types";
+import { IObservable, IStore } from "interfaces";
 
 export class Store<T> implements IStore<T>{
     
-    constructor(private initialData: T,private observable:Observable){}
+    constructor(private initialData: T,private observable:IObservable){}
     
     set(data: Setter<T>){
         if(typeof data === "function"){
@@ -28,15 +28,28 @@ export const createExternalStore = <T>(subscribe: (onStoreChange: Observer) => (
     return external
 }
 
-export function create<T>(initial: T){
-    const observable = new Observable();
-    const store = new Store(initial,observable)
+export function create<T>(initial: T | Persist<T>){
+    let observable: IObservable;
+    let store: IStore<T>;
+
+    const isPersisted = (initial as Persist<T>)?.setItem !== undefined;
+
+    if(isPersisted){
+        let persistedData = initial as Persist<T>;
+        observable = persistedData.observable;
+        store = persistedData.store;
+    }
+    else{
+        observable  = new Observable();
+        store = new Store(initial as T,observable);
+    }
+
     return () => {
         const state = createExternalStore(observable.subscribe.bind(observable),store.get.bind(store))
         
         return {
             state,
-            setState: store.set.bind(store)
+            setState: isPersisted ? (initial as Persist<T>).setItem.bind(store) : store.set.bind(store)
         }
     }
 
